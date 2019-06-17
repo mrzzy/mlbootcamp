fcc05bfb6ab7ad4d1a496b564df365e01ad76ee6
Joel Tio <joeltiojinhon@gmail.com>
2019-06-17 09:31:38 +0800

    Split practicals to answers and no answers

---
+++ practicals_with_ans/day_2/practical_3/model_config.py
@@ -0,0 +1,10 @@
+compile_config = {
+    "optimizer": "adam",
+    "loss": "categorical_crossentropy",
+    "metrics": ["accuracy"],
+}
+
+train_config = {
+    "batch_size": 60,
+    "epochs": 4,
+}
